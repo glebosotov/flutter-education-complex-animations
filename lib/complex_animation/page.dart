92dae7b9529e1f1f8e8419f246338203bfd555c2
glebosotov <gleb.osotov@gmail.com>
2024-05-26 14:16:25 +0300

    feat: add component

--- lib/complex_animation/page.dart
+++ lib/complex_animation/page.dart
@@ -5,6 +5,38 @@ class ComplexAnimationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned.fill(
+              child: _Component(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Component extends StatelessWidget {
+  static const dimension = 200.0;
+
+  const _Component({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: dimension,
+      height: dimension,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/go.png'),
+          fit: BoxFit.cover,
+        ),
+        shape: BoxShape.circle,
+      ),
+    );
   }
 }
